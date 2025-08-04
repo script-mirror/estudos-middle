40ee33113602b86d9b50d07489fb2836fbe51391
GilseuRaizen <gilseu.muhlen@raizen.com>
2025-08-04 10:42:27 -0300

    feat: ajuste get deck

--- estudos_prospec/run_nw_ons_to_ccee.py
+++ estudos_prospec/run_nw_ons_to_ccee.py
@@ -198,9 +198,10 @@ def get_deck_newave(path):
             
     path_deck_nw  = handle_webhook_file(payload, path)
     path_deck_nw  = extract_zip_folder(path_deck_nw, path_deck_nw)
-    parts         = filter_newave_in_dir(path_deck_nw).split('.')[0].split('_')
+    deck_nw       = filter_newave_in_dir(path_deck_nw)
+    parts         = deck_nw.split('.')[0].split('_')
     data_deck     = dt.date(int(parts[2]), int(parts[3]), 1)  
-    path_deck_nw  = path_deck_nw +'/' +os.listdir(path_deck_nw)[0]
+    path_deck_nw  = path_deck_nw +'/' +deck_nw
     path_deck_nw  = extract_zip_folder(path_deck_nw, path_deck_nw)
     return path_deck_nw, data_deck, preliminar
 
