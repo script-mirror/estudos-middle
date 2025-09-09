247505db6edc4d4cc490d399acde44a769bc5249
GilseuRaizen <gilseu.muhlen@raizen.com>
2025-09-09 14:56:34 -0300

    feat; remove save log

--- api_pluvia/run_pluvia.py
+++ api_pluvia/run_pluvia.py
@@ -17,7 +17,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('pluvia_script.log'),
         logging.StreamHandler()
     ]
 )
